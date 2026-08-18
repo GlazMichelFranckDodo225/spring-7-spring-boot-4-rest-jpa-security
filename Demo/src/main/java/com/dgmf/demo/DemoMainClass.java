081663c8df71a33d431056f505e5aa544a5d3aab
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2026-08-18 10:42:36 +0200

    @Configuration and @Bean annotations in action

--- Demo/src/main/java/com/dgmf/demo/DemoMainClass.java
+++ Demo/src/main/java/com/dgmf/demo/DemoMainClass.java
@@ -1,7 +1,35 @@
 package com.dgmf.demo;
 
+import com.dgmf.demo.beans.Vehicle;
+import com.dgmf.demo.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class DemoMainClass {
     static void main() {
-        System.out.println("Hello World");
+            Vehicle vehicle = new Vehicle();
+            vehicle.setName("Audi");
+            System.out.println("Vehicle name from non-spring context is : " + vehicle.getName());
+
+            var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+            var veh = context.getBean(Vehicle.class);
+            System.out.println("Vehicle name from Spring Context is : " + veh.getName());
+
+            /*
+            We don't need to do any explicit casting while fetching a bean from context.
+            Spring is smart enough to look for a bean of the type you requested in its context.
+            If such a bean doesn't exist, Spring will throw an exception
+            */
+            String hello = context.getBean(String.class);
+            System.out.println("String value from Spring Context is : " + hello);
+
+            Integer num = context.getBean(Integer.class);
+            System.out.println("Integer value from Spring Context is : " + num);
+
+            String hello2 = (String) context.getBean("hello");
+            System.out.println("String value from Spring Context is : " + hello2);
+
+            // context.getBean(Double.class); // NoSuchBeanDefinitionException : No
+            // qualifying bean of type 'java.lang.Double' available
+
+        }
     }
-}
---
+++ Demo/src/main/java/com/dgmf/demo/beans/Vehicle.java
@@ -0,0 +1,13 @@
+package com.dgmf.demo.beans;
+
+public class Vehicle {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
